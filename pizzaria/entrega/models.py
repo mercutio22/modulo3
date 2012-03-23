47c0590126c5eb1c7772b2dcd1379279649e3e4d
htorres <mnemonico@posthocergopropterhoc.net>
2012-03-22 23:15:25 -0300

    adicionando broken fixtures para casa

--- pizzaria/entrega/models.py
+++ pizzaria/entrega/models.py
@@ -1,9 +1,19 @@
+#coding=utf-8
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=128)
-    telefone = models.CharField(max_length=16)
+    nome = models.CharField(max_length=128, db_index=True)
+    fone = models.CharField(max_length=16, db_index=True)
+    ramal = models.CharField(max_length=4, blank=True)
     email = models.EmailField(blank=True)
+    logradouro = models.CharField(max_length=32, db_index=True)
+    numero = models.PositiveIntegerField(u'número', db_index=True)
+    complemento = models.CharField(max_length=32, blank=True)
+    obs = models.TextField(verbose_name=u'Observação')
+    
+    class Meta:
+        #Os campos a seguir devem ser únicos concatenados!
+        unique_together = ('fone', 'ramal') 
 
     def __unicode__(self):
         return self.nome
